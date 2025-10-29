159b8dc95f4fc5dddcca9687c356ecb60f3911bd
moslemerror-maker <moslem.error@gmail.com>
2025-10-29 13:20:36 +0530

    Fix CORS origin URL

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ const checkDbConnection = async () => {
 // --- SECURITY (CORS) ---
 // This is the correct, final list of allowed origins.
 const allowedOrigins = [
-    'https://moslemerror-maker.github.io',
+    'https://moslemerror-maker.github.io/asset-app-frontend',
     'https://asset-app-backend-2tgc.onrender.com'
 ];
 
